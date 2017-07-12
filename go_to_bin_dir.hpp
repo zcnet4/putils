badd079d1ce51ee055b2c5708f9c77fef98c2879
Nicolas Phister <nicolas@phister.fr>
2017-07-12 13:02:51 +0200

    followed symbolic links in go_to_bin_dir and added isLink to Directory::File

--- go_to_bin_dir.hpp
+++ go_to_bin_dir.hpp
@@ -5,6 +5,7 @@
 #ifdef __unix__
 
 #include <unistd.h>
+#include <sys/stat.h>
 
 #elif defined(_WIN32)
 
@@ -15,10 +16,27 @@
 namespace putils
 {
     // Go to the directory containing file in currentPath
-    inline void goToBinDir(const std::string &currentPath) noexcept
+    inline void goToBinDir(std::string currentPath)
     {
+#ifdef __unix__
+        struct stat sb;
+        if (lstat(currentPath.data(), &sb) == -1)
+            throw std::runtime_error("Path doesn't exist");
+
+        // If av[0] was a symbolic link, dereference it
+        if (S_ISLNK(sb.st_mode))
+        {
+            char buf[1024];
+            auto ret = readlink(currentPath.c_str(), buf, 1023);
+            if (ret == -1)
+                throw std::runtime_error("Could not read link");
+            buf[ret] = 0;
+            currentPath = buf;
+        }
+#endif
+
         size_t last = currentPath.find_last_of("/\\");
-        const auto dest = currentPath.substr(0, last);
+        auto dest = currentPath.substr(0, last);
         chdir(dest.data());
     }
 }
