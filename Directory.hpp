c4f969db55b43521c70fcbddb5a4ab9819014d22
Nicolas Phister <nicolas@phister.fr>
2017-09-22 11:56:29 +0200

    added tests for Directory

--- Directory.hpp
+++ Directory.hpp
@@ -2,6 +2,7 @@
 
 #include "WindowsDirectory.hpp"
 #include "UnixDirectory.hpp"
+#include "runTests.hpp"
 
 namespace putils
 {
@@ -10,4 +11,37 @@ namespace putils
 #elif defined(__unix__)
     using Directory = UnixDirectory;
 #endif
+
+    namespace test
+    {
+        inline void directory()
+        {
+            putils::runTests(
+                    "for_each", []
+                    {
+                        Directory d(".");
+                        d.for_each([](auto &&f)
+                                   {
+                                       std::cout << f.name << std::endl;
+                                       std::cout << "\t" << f.fullPath << std::endl;
+                                       std::cout << "\t" << (f.isDirectory ? "Dir" : "NotDir")  << std::endl;
+                                   }
+                        );
+                        return true;
+                    },
+                    "getFiles", []
+                    {
+                        Directory d(".");
+                        const auto files = d.getFiles();
+                        for (const auto &f : files)
+                        {
+                            std::cout << f.name << std::endl;
+                            std::cout << "\t" << f.fullPath << std::endl;
+                            std::cout << "\t" << (f.isDirectory ? "Dir" : "NotDir")  << std::endl;
+                        }
+                        return true;
+                    }
+            );
+        }
+    }
 }
