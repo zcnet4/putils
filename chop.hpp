97cd0955d7cdd4b5dfda402462f4344b7ee96da3
Nicolas Phister <nicolas@phister.fr>
2017-09-22 16:25:11 +0200

    added tests for chop

--- chop.hpp
+++ chop.hpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <string_view>
+#include "runTests.hpp"
 
 namespace putils
 {
@@ -13,5 +14,19 @@ namespace putils
             return std::string(str);
         else
             return std::string(str.substr(0, str.find_last_not_of(" \t\n\r") + 1));
+    }
+
+    namespace test
+    {
+        inline bool chop()
+        {
+            return putils::runTests(
+                    "Space", []{ return putils::chop("test ") == "test"; },
+                    "Tab", []{ return putils::chop("test\t") == "test"; },
+                    "Newline", []{ return putils::chop("test\n") == "test"; },
+                    "Carriage return", []{ return putils::chop("test\r") == "test"; },
+                    "All", []{ return putils::chop("test\r \n\t") == "test"; }
+            );
+        }
     }
 }
