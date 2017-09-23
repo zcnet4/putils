7a42c01f3bd1bbe14e46d31778bb6fc37a0a94e7
Nicolas Phister <nicolas@phister.fr>
2017-09-23 18:29:34 +0200

    added RAII tests

---
+++ test/RAII_test.cpp
@@ -0,0 +1,24 @@
+#include "gtest/gtest.h"
+#include "RAII.hpp"
+
+TEST(RAIITest, Release)
+{
+    int i = 0;
+    {
+        putils::RAII<int &> obj(i, [](int &x) { x = 42; });
+    }
+    EXPECT_EQ(i, 42);
+}
+
+TEST(RAIITest, Move)
+{
+    testing::internal::CaptureStdout();
+
+    {
+        putils::RAII<int> obj(42, [](auto &&...) { std::cout << 42; });
+        putils::RAII<int> replacer(std::move(obj));
+    }
+
+    const auto str = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(str, "42");
+}
